cdbf3edb38a87d9c684fbd76de682405fd7de77e
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-08 11:38:31 +0300

    Fix dataset package exports

---
+++ slam_fusion/datasets/__init__.py
@@ -0,0 +1,5 @@
+"""Dataset manifest and loader scaffolds."""
+
+from slam_fusion.datasets.manifests import DatasetManifest, load_manifest, write_manifest_template
+
+__all__ = ["DatasetManifest", "load_manifest", "write_manifest_template"]
